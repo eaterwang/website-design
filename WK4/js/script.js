c27b58d632bb82d10246f8428d2eb75a01ea6ca5
eaterwang <asd11121112@gmail.com>
2021-03-16 22:08:20 +0800

    basic structure

---
+++ WK4/js/script.js
@@ -0,0 +1,41 @@
+var name ="王幃帆台灣之光財神羅伯特歐薩斯";
+
+alert("Hello" + name);
+
+var name ="台灣之光";
+var age = 20;
+var isBoy = false;
+var classmate = ["bratism","george","pechi"];
+
+alert(name); //Yu
+alert(age); //18
+alert(isBoy); //true
+alert(classmate[0]); 
+alert(classmate[2]); 
+alert(classmate[1]); 
+
+function addition(num){
+return num+10;
+}
+var age = 28;
+var trueage= addition(age);
+alert(trueage);//”38”
+
+function test(){
+	a     = 123
+	var b = 456
+	if(a == 123) var c = 789;
+}
+
+document.getElementsByTagName("p")[0].innerHTML = "e04e04e04e04ㄍㄍㄍ";
+document.getElementsByTagName("p")[0].style.color = "salmon";
+
+document.getElementById("byid").innerHTML = "我來自id";
+document.getElementById("byid").style.color = "blue";
+
+document.getElementsByClassName("byclass")[0].innerHTML = "我來自class";
+document.getElementsByClassName("byclass")[0].style.fontSize = "30px";
+
+document.getElementsByTagName("div")[2].innerHTML = "我來自Tag";
+document.getElementsByTagName("div")
+
